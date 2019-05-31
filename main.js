dfdf5955d62a4fffbe9316794d8f3cbf1a931909
FBGFi <50401282+FBGFi@users.noreply.github.com>
2019-05-31 09:27:08 +0300

    Update main.js

--- main.js
+++ main.js
@@ -8,62 +8,80 @@ $(document).ready(function()
     if(i == 1)
     {
       $(".container").children(":nth-child(1)").css('z-index', '-1').animate({
-        left: '70%',
+        left: '60%',
         top: '250px',
-        opacity: 0.5
+        opacity: 0.5,
+        width: '200px',
+        height: '150px'
       }, 700);
       $(".container").children(":nth-child(2)").animate({
         left: '50%',
         top: '200px',
         opacity: 1,
-        zIndex: 1
+        zIndex: 1,
+        width: '400px',
+        height: '300px'
       }, 700);
       $(".container").children(":nth-child(3)").animate({
-        left: '30%',
+        left: '40%',
         top: '250px',
         opacity: 0.5,
-        zIndex: 0
+        zIndex: 0,
+        width: '200px',
+        height: '150px'
       }, 700);
     }
     else if (i == 3)
     {
         $(".container").children(":nth-child(3)").css('z-index', '-1').animate({
-          left: '70%',
+          left: '60%',
           top: '250px',
-          opacity: 0.5
+          opacity: 0.5,
+          width: '200px',
+          height: '150px'
         }, 700);
         $(".container").children(":nth-child(1)").animate({
           left: '50%',
           top: '200px',
           opacity: 1,
-          zIndex: 1
+          zIndex: 1,
+          width: '400px',
+          height: '300px'
         }, 700);
         $(".container").children(":nth-child(2)").animate({
-          left: '30%',
+          left: '40%',
           top: '250px',
           opacity: 0.5,
-          zIndex: 0
+          zIndex: 0,
+          width: '200px',
+          height: '150px'
         }, 700);
         i = 0;
     }
     else
     {
         $(".container").children(":nth-child(2)").css('z-index', '-1').animate({
-          left: '70%',
+          left: '60%',
           top: '250px',
-          opacity: 0.5
+          opacity: 0.5,
+          width: '200px',
+          height: '150px'
         }, 700);
         $(".container").children(":nth-child(3)").animate({
           left: '50%',
           top: '200px',
           opacity: 1,
-          zIndex: 1
+          zIndex: 1,
+          width: '400px',
+          height: '300px'
         }, 700);
         $(".container").children(":nth-child(1)").animate({
-          left: '30%',
+          left: '40%',
           top: '250px',
           opacity: 0.5,
-          zIndex: 0
+          zIndex: 0,
+          width: '200px',
+          height: '150px'
         }, 700);
     }
 
@@ -72,6 +90,6 @@ $(document).ready(function()
       {
         i++;
         next(i);
-      }, 2000);
+      }, 4000);
   }
 });
